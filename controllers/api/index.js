af392417c53dbee80cd972e543bb0fb75b4ae127
sep <sep.alamouti@sepalamouti.com>
2023-11-02 15:56:35 -0700

    updating-controller-auth.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+
+router.use('/users', userRoutes);
+router.use('/dashboard', dashboardRoutes);
+
 
 module.exports = router;
