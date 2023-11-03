ca95e7ce614435b26e90b1c23444ef9e8e316667
sep <sep.alamouti@sepalamouti.com>
2023-11-03 12:33:45 -0700

    seeds-updated-variables-added-multiple-experties

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,81 +1,79 @@
 const sequelize = require('../config/connection');
 const User = require('../models/user');
 const Expertise = require('../models/expertise');
+const UserExpertise = require('../models/userExpertise');
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
 const seedDatabase = async () => {
-    try {
-        await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-        // Expertise Seed
-        const expertiseData = [
-            { languages: 'HTML' },
-            { languages: 'JavaScript' },
-            { languages: 'CSS' },
-            { languages: 'Database' },
-        ];
-        await Expertise.bulkCreate(expertiseData);
+  // Expertise Seed
+  const expertises = await Expertise.bulkCreate([
+    { languages: 'HTML' },
+    { languages: 'JavaScript' },
+    { languages: 'CSS' },
+    { languages: 'Database' },
+  ]);
 
-        // Users Seed
-        const userData = [
-            {
-                userName: 'johnDoe',
-                password: 'password123',
-                email: 'john@example.com',
-                administrator: false,
-                expertise_id: 1,
-            },
-            {
-                userName: 'janeDoe',
-                password: 'password456',
-                email: 'jane@example.com',
-                administrator: true,
-                expertise_id: 2,
-            },
-        ];
-        await User.bulkCreate(userData);
+  // Users Seed
+  const users = await User.bulkCreate([
+    {
+      userName: 'johnDoe',
+      password: 'password123',
+      email: 'john@example.com',
+      administrator: false,
+    },
+    {
+      userName: 'janeDoe',
+      password: 'password456',
+      email: 'jane@example.com',
+      administrator: true,
+    },
+  ]);
 
-        // Posts Seed
-        const postData = [
-            {
-                title: 'HTML Basics',
-                content: 'HTML stands for HyperText Markup Language.',
-                userName_id: 1,
-                post_date: new Date(),
-            },
-            {
-                title: 'Understanding JavaScript',
-                content: 'JavaScript is a scripting language for Web pages.',
-                userName_id: 2,
-                post_date: new Date(),
-            },
-        ];
-        await Post.bulkCreate(postData);
+  // UserExpertise Seed
+  await UserExpertise.bulkCreate([
+    { user_id: 1, expertise_id: 1 },  // John knows HTML
+    { user_id: 1, expertise_id: 2 },  // John also knows JavaScript
+    { user_id: 2, expertise_id: 3 },  // Jane knows CSS
+  ]);
 
-        // Comments Seed
-        const commentData = [
-            {
-                commentText: 'This is very informative, thanks!',
-                commented_at: new Date(),
-                userName_id: 2,
-                post_id: 1,
-            },
-            {
-                commentText: 'I had some doubts, but this cleared it up!',
-                commented_at: new Date(),
-                userName_id: 1,
-                post_id: 2,
-            },
-        ];
-        await Comment.bulkCreate(commentData);
+  // Posts Seed
+  const posts = await Post.bulkCreate([
+    {
+      title: 'HTML Basics',
+      content: 'HTML stands for HyperText Markup Language.',
+      username_id: 1,
+      post_date: new Date(),
+    },
+    {
+      title: 'Understanding JavaScript',
+      content: 'JavaScript is a scripting language for Web pages.',
+      username_id: 2,
+      post_date: new Date(),
+    },
+  ]);
 
-        console.log('Database seeded successfully.');
-    } catch (error) {
-        console.error('Error during database seeding:', error);
-    } finally {
-        process.exit(0);
-    }
+  // Comments Seed
+  const comments = await Comment.bulkCreate([
+    {
+      commentText: 'This is very informative, thanks!',
+      commented_at: new Date(),
+      user_id: 2,
+      post_id: 1,
+    },
+    {
+      commentText: 'I had some doubts, but this cleared it up!',
+      commented_at: new Date(),
+      user_id: 1,
+      post_id: 2,
+    },
+  ]);
+
+  process.exit(0);
 };
 
 seedDatabase();
+
+
