2bb87cbb9ce620d39c75a1f3f1c6c5c6595b049f
Xenodius <xenodius@hotmail.com>
2023-11-07 21:55:54 -0400

    added code for edit and dashboard, will need to expand

--- controllers/edit-routes.js
+++ controllers/edit-routes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const { Post } = require("../models");
+// Justin current working on fixing after edit to stay in current dashboard
+const { User } = require("../models")
+// 
 const withAuth = require("../utils/auth");
 
 //on /edit/{id} page load
@@ -7,13 +10,20 @@ router.get("/:id", withAuth, async (req, res) => {
     console.log('======edit-routes invoked=====');
     try {
         const postData = await Post.findByPk(req.params.id);
+        // Justin current working on fixing after edit to stay in current dashboard
+        const userData = await User.findByPk(req.params.id);
+        //
         const post = postData.get({ plain: true });
+        // Justin current working on fixing after edit to stay in current dashboard
+        const user = userData.get({ plain: true });
+        //
         if (post) {
             res.render("edit", {
                 loggedIn: req.session.loggedIn,
                 loggedInUserData: req.session.loggedInUserData,
                 postData: post,
             });
+            // Justin may need to add code in here for functionality of edit with routes and dashboard
         } else {
             res.redirect("/dashboard");
         }
